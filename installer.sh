bfebc1905e61c24f6b4b8ce7d6cf741885eb45ee
stega <stega@stega.org>
2012-08-29 16:28:47 -0700

    more changes:  trying to debug untar

--- installer.sh
+++ installer.sh
@@ -17,5 +17,6 @@ cp www/placeholder.txt $WEB/
 cp www/style.css $WEB/
 touch $WEB/feed.xml
 touch $WEB/old.xml
-tar -xf dev/magpierss-0.72.tar $WEB
+tar -xf dev/magpierss-0.72.tar 
+mv magpierss-0.72 $WEB/
 ln -s $WEB/magpierss-0.72 $WEB/magpierss
