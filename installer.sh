002d2c658f82f3002db4630eee35792d7b0f9338
stega <stega@stega.org>
2012-08-30 16:26:25 -0700

    debugging

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -v
 #
 # Umbrella 3.0 smart phone to RSS
 # Take a picture or video with your phone and post it to your private (non-cloud) photo/vid blog
@@ -10,13 +10,20 @@
 # Location for www files
 . umbrella/variables
 WEB=$WORKDIR
+echo 'web='$WEB
 mkdir $WEB
+echo 'I have made the web directory'
 cp www/index.php $WEB/
 cp www/placeholder.jpg $WEB/
 cp www/placeholder.txt $WEB/
 cp www/style.css $WEB/
+echo 'I have copied the files'
 touch $WEB/feed.xml
 touch $WEB/old.xml
+echo 'I have touched feed and old'
 tar -xf dev/magpierss-0.72.tar 
+echo 'I have untard magpierss'
 mv magpierss-0.72 $WEB/
+echo 'I have moved magpierss'
 ln -s $WEB/magpierss-0.72 $WEB/magpierss
+echo 'I have made the symlink'
