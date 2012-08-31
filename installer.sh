28cd97807bf54b33cc1de46b59ffe9b37a278282
stega <stega@stega.org>
2012-08-31 16:27:13 -0700

    fixed variables

--- installer.sh
+++ installer.sh
@@ -10,21 +10,22 @@
 # Location for www files
 . umbrella/variables
 WEB=$WORKDIR
-echo 'web='$WEB
-mkdir $WEB
+echo 'web='${WEB}
+mkdir ${WEB}
 echo 'I have made the web directory'
-cp www/placeholder.jpg $WEB/
-cp www/placeholder.txt $WEB/
-cp www/style.css $WEB/
+cp www/placeholder.jpg ${WEB}/
+cp www/placeholder.txt ${WEB}/
+cp www/style.css ${WEB}/
 echo 'I have copied the files'
-touch $WEB/feed.xml
-touch $WEB/old.xml
+touch ${WEB}/feed.xml
+touch ${WEB}/old.xml
 echo 'I have touched feed and old'
 tar -xf dev/magpierss-0.72.tar 
 echo 'I have untard magpierss'
-mv magpierss-0.72 $WEB/
+mv magpierss-0.72 ${WEB}/
 echo 'I have moved magpierss'
-ln -s $WEB/magpierss-0.72 magpierss
+cd ${WEB}
+ln -s magpierss-0.72 magpierss
 echo 'I have made the symlink'
 
 #cat www/index.php | sed -e 's/HOWMANY/${HOW_MANY}/' 's/URL_REPLACE/${URL}/' 's/STYLESHEET_REPLACE/${STYLE}/' > www/new_index.php
