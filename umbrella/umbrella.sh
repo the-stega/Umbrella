d2724c6e831d895778d5e9d5707ec9ecb25a0c91
stega <stega@stega.org>
2012-09-01 20:10:25 -0700

    updated temp file creation

--- umbrella/umbrella.sh
+++ umbrella/umbrella.sh
@@ -12,6 +12,7 @@
 
 cd ${WORKDIR}
 # Spam the message on standard input into a scratch file. Pull out the subject header field, then explode the message into its various parts.
+TEMPFILE=`mktemp ${TEMP_TEMPLATE}`
 cat > ${TEMPFILE}
 SUBJECT=`grep -i \^subject: ${TEMPFILE} | cut -c 10-100`
 ${MUNPACK} -q < ${TEMPFILE} > /dev/null 2> /dev/null
