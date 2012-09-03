a4d0788d502ffaeb4482a4557b2ca31ab2f476ba
stega <stega@stega.org>
2012-09-02 17:13:19 -0700

    added initial test runs to install.sh

--- installer.sh
+++ installer.sh
@@ -6,10 +6,12 @@
 # installer script
 #
 # Set Variables
+. umbrella/variables.txt
 # 
 # Location for www files
-. umbrella/variables.txt
 WEB=$WORKDIR
+#
+# Do the install tasks
 mkdir ${WEB}
 cp www/placeholder.jpg ${WEB}/
 cp www/placeholder.txt ${WEB}/
@@ -18,9 +20,12 @@ touch ${WEB}/feed.xml
 touch ${WEB}/old.xml
 tar -xf dev/magpierss-0.72.tar 
 mv magpierss-0.72 ${WEB}/
-
 (cd ${WEB} && ln -s magpierss-0.72 magpierss)
 (cd ${WEB} && mkdir images)
 (cd ${WEB} && mkdir movies)
-
 cat www/index.php | sed -e "s%HOW_MANY%${HOW_MANY}%" -e "s%URL_REPLACE%${URL}%" -e "s%STYLESHEET_REPLACE%${STYLE}%" > ${WEB}/index.php
+#
+# Now run the test cases to be sure the basics are working (will fail if iphone is not the selected type--will add additional test cases for release)
+(cd umbrella && sh umbrella.sh < ../samples/iphone/iphone-test-01.eml)
+(cd umbrella && sh umbrella.sh < ../samples/iphone/iphone-test-02.eml)
+(cd umbrella && sh umbrella.sh < ../samples/iphone/iphone-test-vid.eml)
