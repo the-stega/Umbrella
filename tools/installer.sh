48e19c947f1a2055034d01a03e5022a4667ef97f
stega <stega@gabriel.local>
2013-05-13 19:17:38 -0700

    fixed lots of paths

--- tools/installer.sh
+++ tools/installer.sh
@@ -40,18 +40,18 @@ WEB=$WORKDIR
 
 # Do the install tasks
 mkdir ${WEB}
-cp www/placeholder.jpg ${WEB}/
-cp www/umbrella-wm.png ${WEB}/
-cp www/style.css ${WEB}/
+cp ../www/placeholder.jpg ${WEB}/
+cp ../www/umbrella-wm.png ${WEB}/
+cp ../www/style.css ${WEB}/
 touch ${WEB}/feed.xml
 touch ${WEB}/old.xml
-tar -xf dev/magpierss-0.72.tar 
+tar -xf ../dev/magpierss-0.72.tar 
 mv magpierss-0.72 ${WEB}/
 (cd ${WEB} && ln -s magpierss-0.72 magpierss)
 (cd ${WEB} && mkdir images)
 (cd ${WEB} && mkdir movies)
 echo "I have put all the appropriate files in your WORKDIR location"
-cat www/index.php | sed -e "s%HOW_MANY%${HOW_MANY}%" -e "s%URL_REPLACE%${URL}%" -e "s%STYLESHEET_REPLACE%${STYLE}%" > ${WEB}/index.php
+cat ../www/index.php | sed -e "s%HOW_MANY%${HOW_MANY}%" -e "s%URL_REPLACE%${URL}%" -e "s%STYLESHEET_REPLACE%${STYLE}%" > ${WEB}/index.php
 echo "I have set the values for index.php and placed it in your WORKDIR"
 #
 # Now run the test cases to be sure the basics are working (will fail if iphone is not the selected type--will add additional test cases for release)
