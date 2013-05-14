641dfb3129cad428fb8804495a0f4d2eeeae7a0c
stega <stega@gabriel.local>
2013-05-13 19:37:50 -0700

    started mail stuff

--- tools/trackback.sh
+++ tools/trackback.sh
@@ -2,15 +2,16 @@
 #iPhone support
 #
 TRACKBACK_WRITE() { 
-if [ ${LINK}x = "x" ]; then
-    return
-fi
-MSUBJECT="Post from your Phone at ${TITLESTAMP}"
-EMAILMESSAGE="${WORKDIR}/scratch/emailmessage.txt"
-echo "Howdy, your post has been successfully received:"> $EMAILMESSAGE
-echo "${SUBJECT}"> $EMAILMESSAGE
-echo "You can use this link to share your post with others:" >>$EMAILMESSAGE
-echo "${LINK}" >>$EMAILMESSAGE
-# send an email using /bin/mail
-/bin/mail -s "$MSUBJECT" "$EMAIL" < $EMAILMESSAGE
+	if [ ${LINK}x = "x" ]; then
+		echo "No link to trackback"
+	    return
+	fi
+	MAIL_SUBJECT="Post from your Phone at ${TITLESTAMP}"
+	EMAILMESSAGE="${WORKDIR}/scratch/emailmessage.txt"
+	echo "Howdy, your post has been successfully received:"> $EMAILMESSAGE
+	echo "${SUBJECT}"> $EMAILMESSAGE
+	echo "You can use this link to share your post with others:" >>$EMAILMESSAGE
+	echo "${LINK}" >>$EMAILMESSAGE
+	# send an email using /bin/mail
+	/bin/mail -s "$MAIL_SUBJECT" "$EMAIL" < $EMAILMESSAGE
 }
