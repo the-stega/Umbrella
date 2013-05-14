443535e223da9140ed7475f0523d827be480a6df
stega <stega@gabriel.local>
2013-05-13 19:09:08 -0700

    debugging file type

--- tools/file_handler.sh
+++ tools/file_handler.sh
@@ -3,16 +3,16 @@
 DO_FILE() {
     FILETYPE=`/usr/bin/file -bi ${LARGEFILE}`
     case $FILETYPE in
-        image/jpeg )
+        image/jpeg* )
             JPG_OUTPUT
             MODE=image
             ;;
-         video/3gpp )
+         video/3gpp* )
             MOV_SUFF=3gp
             MOV_OUTPUT
             MODE=movie 
             ;;
-         video/quicktime )
+         video/quicktime* )
             MOV_SUFF=mov
             MOV_OUTPUT
             MODE=movie 
